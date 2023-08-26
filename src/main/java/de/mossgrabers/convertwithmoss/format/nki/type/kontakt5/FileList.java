4e51b4450478098a0c05f93f5e5ee3ab9c463ff9
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2023-08-27 00:01:14 +0200

    Fixes and cleanup Kontakt 5+.

--- src/main/java/de/mossgrabers/convertwithmoss/format/nki/type/kontakt5/FileList.java
+++ src/main/java/de/mossgrabers/convertwithmoss/format/nki/type/kontakt5/FileList.java
@@ -38,12 +38,11 @@ public class FileList
         final byte [] data = chunk.getPublicData ();
         final ByteArrayInputStream in = new ByteArrayInputStream (data);
 
-        final int version = StreamUtils.readUnsigned16 (in, false);
-        if (version > 2)
-            throw new IOException ("TODO"); // TODO
-
         if (chunkID == PresetChunkID.FILENAME_LIST_EX)
         {
+            final int version = StreamUtils.readUnsigned16 (in, false);
+            if (version != 2)
+                throw new IOException (Functions.getMessage ("IDS_NKI5_UNSUPPORTED_FILELIST_EX_VERSION", Integer.toString (version)));
             // Unknown
             StreamUtils.readUnsigned32 (in, false);
         }
@@ -60,6 +59,20 @@ public class FileList
      */
     private void readPublicData (final ByteArrayInputStream in) throws IOException
     {
+        final int version = StreamUtils.readUnsigned32 (in, false);
+        if (version > 1)
+            throw new IOException (Functions.getMessage ("IDS_NKI5_UNSUPPORTED_FILELIST_VERSION", Integer.toString (version)));
+
+        if (version == 1)
+        {
+            // Read the resource container (NKR file)
+            final StringBuilder sb = new StringBuilder ();
+            parseSegment (in, sb);
+
+            // Padding?
+            StreamUtils.readUnsigned32 (in, false);
+        }
+
         final int fileCount = StreamUtils.readUnsigned32 (in, false);
 
         for (int i = 0; i < fileCount; i++)
@@ -67,44 +80,40 @@ public class FileList
             final StringBuilder sb = new StringBuilder ();
             final int segmentCount = StreamUtils.readUnsigned32 (in, false);
             for (int s = 0; s < segmentCount; s++)
-            {
-                final int segmentType = in.read ();
-                switch (segmentType)
-                {
-                    case 0:
-                        throw new IOException ("Found segmentType: 0");
-                    // Unknown
-                    // in.read ();
-                    // in.read ();
-                    // break;
-
-                    case 2:
-                        final String pathSegment = StreamUtils.readWithLengthUTF16 (in);
-                        sb.append (pathSegment).append ('/');
-                        break;
-
-                    case 4:
-                        final String pathSegment2 = StreamUtils.readWithLengthUTF16 (in);
-                        sb.append (pathSegment2);
-                        break;
-
-                    case 6:
-                        throw new IOException ("Found segmentType: 6");
-                    // uint8 "?"
-                    // set length [uint32 "len"]
-                    // utf16 [expr $length * 2] "name"
-                    // break;
-
-                    default: // TODO
-                        throw new IOException ("unknown segmentType: " + segmentType);
-                }
-            }
+                parseSegment (in, sb);
 
             this.filePaths.add (sb.toString ());
         }
 
-        // TODO there are more bytes...
-        int rest = in.available ();
+        // There are much more bytes available...
+    }
+
+
+    /**
+     * Parses a segment of a file path.
+     *
+     * @param in Where to read the segment from
+     * @param sb Where to append the read path segment
+     * @throws IOException Could not read
+     */
+    private static void parseSegment (final ByteArrayInputStream in, final StringBuilder sb) throws IOException
+    {
+        final int segmentType = in.read ();
+        switch (segmentType)
+        {
+            case 2:
+                final String pathSegment = StreamUtils.readWithLengthUTF16 (in);
+                sb.append (pathSegment).append ('/');
+                break;
+
+            case 4:
+                final String pathSegment2 = StreamUtils.readWithLengthUTF16 (in);
+                sb.append (pathSegment2);
+                break;
+
+            default:
+                throw new IOException (Functions.getMessage ("IDS_NKI5_UNSUPPORTED_FILELIST_SEGMENT", Integer.toString (segmentType)));
+        }
     }
 
 
