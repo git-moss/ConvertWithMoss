a15e20f6efd2af50711e445f9276372aae1ee35a
Jürgen Moßgraber <juergen.mossgraber@googlemail.com>
2025-07-05 19:57:33 +0200

    Integrated the macOS section into the main document. Fixed the CLI version info.

--- documentation/build-documentation.cmd
+++ documentation/build-documentation.cmd
@@ -1,3 +1,3 @@
 set SOURCE_PATH=C:\Privat\Programming\ConvertWithMoss\ConvertWithMoss\trunk
 
-docker run --rm -v "%SOURCE_PATH%:/data" pandoc/latex --standalone --toc -V geometry:margin=2.5cm --number-sections --output=ConvertWithMoss-Manual.pdf documentation/README.md documentation/README-MACOS.md documentation/README-FORMATS.md documentation/CHANGELOG.md
+docker run --rm -v "%SOURCE_PATH%:/data" pandoc/latex --standalone --toc -V geometry:margin=2.5cm --number-sections --output=ConvertWithMoss-Manual.pdf documentation/README.md documentation/README-FORMATS.md documentation/CHANGELOG.md
