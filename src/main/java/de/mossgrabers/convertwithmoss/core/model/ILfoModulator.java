5e868c63098c112154e9662babe22751c0ec0a00
Douglas Carmichael <dcarmich@dcarmichael.net>
2026-07-28 08:27:02 -0400

    Added support for a volume LFO (tremolo): DecentSampler, DLS, SFZ, SoundFont 2 (#240)

--- src/main/java/de/mossgrabers/convertwithmoss/core/model/ILfoModulator.java
+++ src/main/java/de/mossgrabers/convertwithmoss/core/model/ILfoModulator.java
@@ -6,12 +6,17 @@ package de.mossgrabers.convertwithmoss.core.model;
 
 /**
  * Interface to a low frequency oscillator modulator. The modulation depth of the pitch modulation
- * maps to -4800..4800 cent, which is the same range as the one of the pitch envelope modulator.
+ * maps to -4800..4800 cent, which is the same range as the one of the pitch envelope modulator. The
+ * modulation depth of the volume modulation maps to -96..96 dB.
  *
  * @author Jürgen Moßgraber
  */
 public interface ILfoModulator extends IModulator
 {
+    /** The maximum depth of a volume modulation in dB. */
+    public static final int MAX_VOLUME_DEPTH = 96;
+
+
     /**
      * Get the modulation source.
      *
